5d1e993639bbebe9ff2bfba64503bddd1b7efae3
v-platon <61975612+v-platon@users.noreply.github.com>
2022-04-17 19:07:27 +0300

    Update

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 now=$(date "+%Y-%m-%d")
-dir=$(pwd)
-pg_dump database > backup_directory/database-$now.dump 1>/var/log/backup/backup-$now.log 2>/var/log/backup/backup-$now-erros.log
+mkdir /var/log/backup
+pg_dump --dbname=postgresql://database_user:1@localhost/database > /home/sadmin/backup_directory/database-$now.dump 1 > /var/log/backup/backup-$now.log 2 > /var/log/backup/backup-$now-erros.log
 
-crontab -l > foocron
-echo "0 0 * * * rm -rf $dir/script.sh" >> foocron
-crontab foocron
-rm foocron
+#crontab -l > foocron
+#echo "0 0 * * * rm -rf /home/sadmin/backup-postgre-4-5/script.sh" >> foocron
+#crontab foocron
+#rm foocron
